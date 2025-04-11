bf2cffe9dd31ee44b17b456b01539c88ee120a4a
Ishaan-Chaturvedi <ishaanc2k2@gmail.com>
2025-04-11 20:35:20 +0530

    building noise simullation using aer

--- src/channels/noisechannel.py
+++ src/channels/noisechannel.py
@@ -1,25 +1,17 @@
 from qiskit_aer import AerSimulator
 from qiskit_aer.noise import NoiseModel, depolarizing_error, amplitude_damping_error
 from qiskit import transpile
-from qiskit import QuantumCircuit
-from qiskit.primitives import BackendSamplerV2
 
-qc=QuantumCircuit(2)
-qc.h(0)
-qc.cx(0,1)
-
-def applynoise(circuit):
-    noisemodel=NoiseModel()
+def depolarizing_noise(p1=0.05,p2=0.05):
     
-    noisemodel.add_all_qubit_quantum_error(depolarizing_error(0.05,1),['h'])
-    noisemodel.add_all_qubit_quantum_error(depolarizing_error(0.05,2),['cx'])
+      noisemodel=NoiseModel()
+      noisemodel.add_all_qubit_quantum_error(depolarizing_error(p1,1),['h'])
+      noisemodel.add_all_qubit_quantum_error(depolarizing_error(p2,2),['cx'])
+      return noisemodel
+
+    
+def applynoise(circuit,noisemodel):
+    
     simulator=AerSimulator(noise_model=noisemodel)
     transpiled_circuit = transpile(circuit, backend=simulator)
     return transpiled_circuit,simulator
-circ,sim=applynoise(qc)
-
-circ.measure_all()
-sampler = BackendSamplerV2(backend=sim)
-job = sampler.run([circ])
-result = job.result()
-print(result[0])
