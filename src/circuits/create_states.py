0c53b062a0926bcfc4dc9fafba513a91493f0bc4
Ishaan-Chaturvedi <ishaanc2k2@gmail.com>
2025-04-11 20:34:34 +0530

    creating circuits

---
+++ src/circuits/create_states.py
@@ -0,0 +1,19 @@
+from qiskit import QuantumCircuit
+
+def bellstategenerator():
+    """
+    Generates a 2-qubit Bell state circuit.
+
+    Returns:
+        QuantumCircuit: Circuit that prepares the Bell state.
+    """
+    bellqc = QuantumCircuit(2)
+    bellqc.h(0)       
+    bellqc.cx(0, 1)   
+    bellqc.draw()
+    return bellqc
+
+# Create the circuit
+qc = bellstategenerator()
+
+
