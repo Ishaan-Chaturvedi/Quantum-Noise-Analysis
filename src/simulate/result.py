58a62acfa8f7fd0be888bfb53bf794b417307eda
Ishaan-Chaturvedi <ishaanc2k2@gmail.com>
2025-04-11 20:43:50 +0530

    result interpretaion

---
+++ src/simulate/result.py
@@ -0,0 +1,21 @@
+from qiskit.quantum_info import state_fidelity
+
+def interpret_result(result):
+    dist=result.quasi_dists[0]
+    print("Measurement outcomes (Probability distribution)->")
+    for outcome, prob in dist.items():
+        print(f"{outcome}:{prob:.4f}")
+
+def compare_result(ideal_result,noisy_result):
+    ideal_dist=ideal_result.quasi_dists[0]
+    noisy_dist=noisy_result.quasi_dists[0]
+
+    print("Measurement outcomes 1 (Probability distribution)->")
+    for outcome, prob in ideal_dist.items():
+        print(f"{outcome}:{prob:.4f}")
+
+    print("Measurement outcomes 1 (Probability distribution)->")
+    for outcome, prob in noisy_dist.items():
+        print(f"{outcome}:{prob:.4f}")
+    
+
