9ff51729f0998c531487106c744c4e22e5739ff8
Ishaan-Chaturvedi <ishaanc2k2@gmail.com>
2025-04-11 22:55:46 +0530

    no quasi dist in new version

--- src/simulate/result.py
+++ src/simulate/result.py
@@ -1,14 +1,17 @@
 from qiskit.quantum_info import state_fidelity
 
 def interpret_result(result):
-    dist=result.quasi_dists[0]
+    #dist = result.counts[0]
     print("Measurement outcomes (Probability distribution)->")
-    for outcome, prob in dist.items():
-        print(f"{outcome}:{prob:.4f}")
+    print(result[0].data.meas.get_counts())
 
+    
+    #for outcome, prob in dist.items():
+      #print(f"{outcome}:{prob:.4f}")
+"""
 def compare_result(ideal_result,noisy_result):
-    ideal_dist=ideal_result.quasi_dists[0]
-    noisy_dist=noisy_result.quasi_dists[0]
+    ideal_dist=ideal_result.quasi_dists(0)
+    noisy_dist=noisy_result.quasi_dists(0)
 
     print("Measurement outcomes 1 (Probability distribution)->")
     for outcome, prob in ideal_dist.items():
@@ -17,6 +20,6 @@ def compare_result(ideal_result,noisy_result):
     print("Measurement outcomes 1 (Probability distribution)->")
     for outcome, prob in noisy_dist.items():
         print(f"{outcome}:{prob:.4f}")
-
+"""
 
 
