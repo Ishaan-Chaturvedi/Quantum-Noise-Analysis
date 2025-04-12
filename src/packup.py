0e6b79fd5bbfc91706d62977e2e89657fe811ec0
Ishaan-Chaturvedi <ishaanc2k2@gmail.com>
2025-04-12 20:55:13 +0530

    adding a function for simulation without noise

--- src/packup.py
+++ src/packup.py
@@ -2,12 +2,15 @@
 from circuits.create_states import bellstategenerator
 from channels.noisechannel import depolarizing_noise
 from circuits.apply_noise import applynoise
-from simulate.simulate import simulatecircuit
+from simulate.simulate import simulatenoise,simulateideal
 from simulate.result import interpret_result
 from Plotting_fn.visualize import histogrammaker
+
+
 state=bellstategenerator()
 noise_model=depolarizing_noise()
 alter_state,sim=applynoise(state,noise_model)
-result=simulatecircuit(alter_state,sim)
-interpret_result(result)
-histogrammaker(result)
+result=simulatenoise(alter_state,sim)
+ideal_result=simulateideal(state)
+interpret_result(ideal_result)
+histogrammaker(ideal_result)
--- src/simulate/simulate.py
+++ src/simulate/simulate.py
@@ -1,7 +1,27 @@
 from qiskit.primitives import BackendSamplerV2
+from qiskit_aer import AerSimulator
+from qiskit import transpile
 
-def simulatecircuit(circuit, simulator):
+
+
+def simulatenoise(circuit, simulator):
     sampler=BackendSamplerV2(backend=simulator)
     job=sampler.run([circuit])
     result=job.result()
     return result
+
+def simulateideal(circuit):
+    """
+    Parameters:
+          circuit
+    Return:
+          Primitiveresult file
+    """
+    
+    simulator=AerSimulator()
+    transpiled_circuit=transpile(circuit,simulator)
+    sampler=BackendSamplerV2(backend=simulator)
+    job=sampler.run([transpiled_circuit])
+    result=job.result()
+    return result
+
