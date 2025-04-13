ff5dbde53f6e4c0cdee041f59d9ad53251e55012
Ishaan-Chaturvedi <ishaanc2k2@gmail.com>
2025-04-13 23:42:28 +0530

    note added trace, need to change generate pdf to work with density matrix

--- src/packup.py
+++ src/packup.py
@@ -2,24 +2,26 @@
 from circuits.create_states import bellstategenerator,ghzstategenerator,teleportationcircuit
 from channels.noisechannel import depolarizing_noise,amplitude_damping,bit_flip
 from simulate.simulate import simulate_circuit
-from simulate.result import interpret_result, measurement_fidelity, operator_fidelity
+from simulate.result import interpret_result, measurement_fidelity, operator_fidelity,measurement_trace,operator_trace
 from result_utils.plotting_result import histogrammaker,circuit_drawer,histcomp
 from result_utils.pdfmaker import generate_pdf_report
 
 
 
-circuit=teleportationcircuit()
+circuit=bellstategenerator(measure=False)
 #state=ghzstategenerator()
 noise_model=depolarizing_noise(p1=0.3,p2=0.2)
-#noise_result=simulate_circuit(circuit,noisemodel=noise_model,method='density_matrix')
-#ideal_result=simulate_circuit(circuit,method='density_matrix')
+noise_result=simulate_circuit(circuit,noisemodel=noise_model,method='density_matrix')
+ideal_result=simulate_circuit(circuit,method='density_matrix')
 #interpret_result(noise_result,prob=True,show=True)
 #fid=operator_fidelity(ideal_result,noise_result)
 #print(fid)
 
 #interpret_result(ideal_result)
 #histcomp(ideal_result,noise_result)
-generate_pdf_report(circuit,noise_model)
+print(operator_trace(noise_result,ideal_result))
+
+#generate_pdf_report(circuit,noise_model)
 #circuitvis(stat
 
 
--- src/simulate/result.py
+++ src/simulate/result.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import sqrtm
 from qiskit.quantum_info import state_fidelity
 
 def interpret_result(result,prob=False,key='meas',show=True):
@@ -44,7 +45,22 @@ def operator_fidelity(result1,result2):
     sigma = result2.data(0)['density_matrix']
     # Step 6: Compute quantum fidelity
     fidelity = state_fidelity(rho, sigma)
-
     return fidelity
-   
+
+def measurement_trace(result1,result2):
+    prob1=interpret_result(result1,prob=True,show=False)
+    prob2=interpret_result(result2,prob=True,show=False) 
+    all_keys = set(prob1.keys()).union(prob2.keys())
+    trace = 0.5 * sum(abs(prob1.get(k, 0) - prob2.get(k, 0)) for k in all_keys)
+    return trace
+
+def operator_trace(result1,result2):
+    rho = result1.data(0)['density_matrix']
+    sigma = result2.data(0)['density_matrix']
+    delta = rho.data - sigma.data
+    sqrt_delta_dagger_delta = sqrtm(delta.conj().T @ delta)
+    trace_norm = np.trace(sqrt_delta_dagger_delta).real
+    return 0.5 * trace_norm
+
+
 
